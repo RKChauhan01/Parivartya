dde51a6f15c47ee601c002bb3e39fb9d8da4d7c3
RKCHAUHAN001 <rakeshkraj2005@gmail.com>
2026-07-30 22:03:15 +0530

    initialize commit

--- slide.js
+++ slide.js
@@ -3,6 +3,7 @@
 ========================================= */
 const slides = document.querySelectorAll(".hero-slide");
 
+const dots = document.querySelectorAll(".dot");
 
 let currentSlide = 0;
 /* =========================================P
